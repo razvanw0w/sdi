d9fbedbb9000e9a89d5321a6e06e2efab280c10d
Horațiu Udrea <udrea.horatiu@gmail.com>
2020-03-10 13:33:58 +0200

    Added stub serializers

---
+++ src/main/java/ro/sdi/lab24/model/serialization/xml/ClientXMLSerializer.java
@@ -0,0 +1,19 @@
+package ro.sdi.lab24.model.serialization.xml;
+
+import org.w3c.dom.Element;
+import ro.sdi.lab24.model.Client;
+
+public class ClientXMLSerializer implements XMLSerializer<Client>
+{
+    @Override
+    public Element serialize(Client entity)
+    {
+        return null;//TODO
+    }
+
+    @Override
+    public Client deserialize(Element element)
+    {
+        return null;//TODO
+    }
+}
---
+++ src/main/java/ro/sdi/lab24/model/serialization/xml/RentalXMLSerializer.java
@@ -0,0 +1,19 @@
+package ro.sdi.lab24.model.serialization.xml;
+
+import org.w3c.dom.Element;
+import ro.sdi.lab24.model.Rental;
+
+public class RentalXMLSerializer implements XMLSerializer<Rental>
+{
+    @Override
+    public Element serialize(Rental entity)
+    {
+        return null;//TODO
+    }
+
+    @Override
+    public Rental deserialize(Element element)
+    {
+        return null;//TODO
+    }
+}
